f8e6fe1b2cfe0b0f95cac50dc84426e7945ca69a
Caroline <caro713f@stud.kea.dk>
2023-02-28 15:06:56 +0000

    images

--- recipes_script.js
+++ recipes_script.js
@@ -1,42 +1,71 @@
 window.addEventListener("DOMContentLoaded", start);
 
-function start(){
-    const jsonData = "recipes.json";
+let recipes;
+let filter = "All";
+const jsonData = "recipes.json";
 
-    getRecipes(jsonData);
+// First function to call after the DOM has loaded
+function start(){
+    console.log("DOM has loaded");
+   
+    // Creating listener for the filter buttons
+    document.querySelectorAll(".filter").forEach(button => {
+        button.addEventListener("click", selectFilter)
+    });
+    getData();
 }
 
-async function getRecipes(jsonData) {
+// Setting the filter after the chosen filter button
+function selectFilter(){
+    filter = this.dataset.filter;
+    console.log(filter);
+    showRecipes();
+    document.querySelector("#recipes_category").textContent = filter;
+}
+
+// Getting data from JSON 
+async function getData() {
     console.log("getting data");
     const result = await fetch(jsonData);
-    const json = await result.json();
-    showRecipes(json);
+    recipes = await result.json();
+    showRecipes();
 }
 
-function showRecipes(recipes){
-    console.log("showing data");
-    const container = document.querySelector("#recipes_wrapper");
-    const template = document.querySelector("template");
-    
+const container = document.querySelector("#recipes_wrapper");
+const template = document.querySelector("template");
+
+function showRecipes(){
+    console.log("showing data");   
+    container.textContent = "";
+
     recipes.forEach(recipe => {
-        console.log("cloning and appending data")
-        const clone = template.cloneNode(true).content;
-        clone.querySelector(".title").textContent = recipe.title;
-        clone.querySelector(".text").textContent = recipe.short_description;
-        clone.querySelector(".text2").textContent = recipe.long_description;
-
-        recipe.ingredients.forEach(ingredient =>{
-            console.log("getting ingredients");
-            clone.querySelector(".ingredients").innerHTML += "<li>" + ingredient + "</li>"
-        });
-
-        clone.querySelector("article").addEventListener("click", () => showRecipe(recipe));
+        if (filter == recipe.category || filter == "All"){
+            const clone = template.cloneNode(true).content;
+            clone.querySelector(".recipe_img").src = `./images/` + recipe.image;
+            clone.querySelector(".category").textContent = recipe.category;
+            clone.querySelector(".time").textContent = recipe.overall_time;
+            clone.querySelector(".title").textContent = recipe.title;
+            clone.querySelector(".text").textContent = recipe.short_description;
+            
+            clone.querySelector("article").addEventListener("click", () => showRecipe(recipe));
 
         container.appendChild(clone);
+        }
+        
+        // clone.querySelector(".text2").textContent = recipe.long_description;
+
+        // recipe.ingredients.forEach(ingredient =>{
+        //     console.log("getting ingredients");
+        //     clone.querySelector(".ingredients").innerHTML += "<li>" + ingredient + "</li>"
+        // });
+
+        
     });
+}
 
     function showRecipe(recipe) {
         console.log(recipe.title + " was clicked")
         location.href = `recipe.html?id=${recipe.title}`;
     }
-}
+// }
+
--- singe_view_script.js
+++ singe_view_script.js
@@ -16,8 +16,6 @@ async function getRecipes(jsonData) {
 
 function showRecipes(recipes){
     console.log("showing data");
-    // const container = document.querySelector("#recipes_wrapper");
-    // const template = document.querySelector("template");
     
     recipes.forEach(recipe => {
         if (recipe.title == id){
@@ -25,19 +23,7 @@ function showRecipes(recipes){
             document.querySelector("#recipe_title").textContent = recipe.title;
             document.querySelector("#recipe_text").textContent = recipe.long_description;
         }
-        // console.log("cloning and appending data")
-        // const clone = template.cloneNode(true).content;
-        // clone.querySelector(".title").textContent = recipe.title;
-        // clone.querySelector(".text").textContent = recipe.short_description;
-        // clone.querySelector(".text2").textContent = recipe.long_description;
-
-        // recipe.ingredients.forEach(ingredient =>{
-        //     console.log("getting ingredients");
-        //     clone.querySelector(".ingredients").innerHTML += "<li>" + ingredient + "</li>"
-        // });
-
-        // clone.querySelector("article").addEventListener("click", () => showRecipe(recipe));
-
-        // container.appendChild(clone);
     });
 }
+
+
